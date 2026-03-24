88856e3f2deac0b09599a943379d7dbb1099016b
allanalvesrodrigues <iallan_alves@outlook.com.br>
2026-03-24 17:40:12 -0300

    Não vamos mais utilizar PRISMA, Retirado tudo sobre Prisma e agora irá ser no SQL Puro.

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const pool = require("./db");
 
 const app = express();
 
-app.use(express.json());
-
-// rota de teste
-app.get("/", (req, res) => {
-  res.send("Servidor rodando 🚀");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro no banco");
+  }
 });
 
 app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+  console.log("Servidor rodando 🚀");
 });
